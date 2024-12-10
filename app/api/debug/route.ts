3694cda94bbf2d1069c50648c2c8c06fd07b8bb4
bamkadayat <bamkadayat@gmail.com>
2024-12-10 23:21:51 +0100

    Add binaryTargets for rhel-openssl-1.0.x

--- app/api/debug/route.ts
+++ app/api/debug/route.ts
@@ -4,8 +4,10 @@ import { NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function GET() {
+    console.log("Environment Variables in Production:");
+    console.log("DATABASE_URL:", process.env.DATABASE_URL);
+    console.log("NODE_ENV:", process.env.NODE_ENV);
   try {
-    // Test database connection
     const result = await prisma.$queryRaw`SELECT 1;`;
     return NextResponse.json({ message: "Database connection successful", result });
   } catch (error) {
